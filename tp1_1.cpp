6a838cea43420e76807c57692e894f6aaacad45e
BaltaRisso <rissopatronivan62@gmail.com>
2024-04-03 12:38:57 -0300

    punto e) trabajo con punteros

--- tp1_1.cpp
+++ tp1_1.cpp
@@ -2,6 +2,15 @@
 
 int main()
 {
-    printf("Hola mundo");
+    printf("Hola mundo\n");
+    int aux = 5;
+    int* puntero = &aux;
+
+    printf("El contenido del puntero es: %d\n", *puntero);
+    printf("Direccion de memoria almacenada por el puntero: %d\n", puntero);
+    printf("Direccion de memoria de la variable: %d\n", &aux);
+    printf("Direccion de memoria del puntero: %d\n", &puntero);
+    printf("Tamanio de memoria utilizado por la variable: %d\n", sizeof(aux));
+
     return 0;
 }
